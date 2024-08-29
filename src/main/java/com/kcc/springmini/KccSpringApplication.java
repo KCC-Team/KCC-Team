45312aa5e942f424a071120b162c163f8e9df53b
angelSuho <angelsuho0303@gmail.com>
2024-08-29 11:50:20 +0900

    project init

---
+++ src/main/java/com/kcc/springmini/KccSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kcc.springmini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KccSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KccSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kcc/springmini/KccSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcc.springmini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KccSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
