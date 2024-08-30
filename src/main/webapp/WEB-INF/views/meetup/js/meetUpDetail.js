a82a45e5e7553c1aa1cb58c060508de82617aa9a
hanheeland <susunnn48@gmail.com>
2024-08-30 09:03:48 +0900

    create mypage view

--- src/main/webapp/WEB-INF/views/meetup/js/meetUpDetail.js
+++ src/main/webapp/WEB-INF/views/meetup/js/meetUpDetail.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
   $(".meetUpmodal").hide();
+
   $(".schedule").click(function () {
     $(".meetUpmodal").trigger("click");
   });
+
+  $(".posts").click(function () {
+    location.href="./postDetail.html"
+  });
+
 });
