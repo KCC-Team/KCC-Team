4eae60a305d80a218f589acb0dcda6553a5fdbeb
Hanhee Lee <42083693+hanheeland@users.noreply.github.com>
2024-08-30 18:00:42 +0900

    [#26] 파일 경로 수정 (#28)
    * create mypage view
    
    * modify file path

--- src/main/java/com/kcc/springmini/domain/member/controller/MemberController.java
+++ src/main/java/com/kcc/springmini/domain/member/controller/MemberController.java
@@ -1,4 +1,24 @@
 package com.kcc.springmini.domain.member.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MemberController {
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "member/loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "member/joinForm";
+    }
+
+    @GetMapping("/mypage")
+    public String mypage() {
+        return "member/mypage";
+    }
+
 }
