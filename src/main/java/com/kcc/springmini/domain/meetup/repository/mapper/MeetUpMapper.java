c7a3e3016140afcaecbc09722a6a6b4b65959ce5
kimyeonho <dksk87721@gmail.com>
2024-09-01 10:38:49 +0900

    모임에 속한 총 회원수 구하기

--- src/main/java/com/kcc/springmini/domain/meetup/repository/mapper/MeetUpMapper.java
+++ src/main/java/com/kcc/springmini/domain/meetup/repository/mapper/MeetUpMapper.java
@@ -1,4 +1,8 @@
 package com.kcc.springmini.domain.meetup.repository.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface MeetUpMapper {
+    int getMemberTotal(Long meetUpId);
 }
--- src/main/java/com/kcc/springmini/domain/meetup/service/MeetUpServiceImpl.java
+++ src/main/java/com/kcc/springmini/domain/meetup/service/MeetUpServiceImpl.java
@@ -1,4 +1,17 @@
 package com.kcc.springmini.domain.meetup.service;
 
-public class MeetUpServiceImpl {
+import com.kcc.springmini.domain.meetup.repository.mapper.MeetUpMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MeetUpServiceImpl implements MeetUpService{
+
+    private final MeetUpMapper mapper;
+
+    @Override
+    public int getMemberTotal(Long meetUpId) {
+        return mapper.getMemberTotal(meetUpId);
+    }
 }
