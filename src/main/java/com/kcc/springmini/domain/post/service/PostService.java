f4ff215625e0991e2bf543535e960068f74bd100
kimyeonho <dksk87721@gmail.com>
2024-09-01 10:54:15 +0900

    모임의 총 게시글 및 총 갯수 구하기

--- src/main/java/com/kcc/springmini/domain/post/service/PostService.java
+++ src/main/java/com/kcc/springmini/domain/post/service/PostService.java
@@ -1,4 +1,9 @@
 package com.kcc.springmini.domain.post.service;
 
+import com.kcc.springmini.domain.post.model.vo.PostVO;
+
+import java.util.List;
+
 public interface PostService {
+    public List<PostVO> findAll(Long meetUpId);
 }
